abb34a444c80c25347ba7fe444b52bc94be4c328
Michell Quinonez <michell@netmx.com.mx>
2026-03-20 07:39:31 -0600

    Add Tailwind CSS integration and new application pages
    - Introduced Tailwind CSS by adding configuration files and importing styles.
    - Updated package.json and package-lock.json to include Tailwind CSS and PostCSS dependencies.
    - Created new pages: About, Contact, and Pricing with corresponding components and templates.
    - Implemented a Navbar component for navigation between pages.
    - Updated routing to include new pages and a default redirect to About.
    - Added VSCode settings for Angular schematics.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about-page'),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact-page'),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () => import('./pages/pricing/pricing-page'),
+  },
+
+  {
+    path: '**',
+    redirectTo: ()=> {
+      //const authService = inject(AuthService);
+
+      return 'about';
+    }
+  }
+];
